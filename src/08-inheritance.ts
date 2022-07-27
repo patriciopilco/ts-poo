3a8a0f99aa31237677223d9473fa75b68521a3f8
pattopilco <pattopilco@gmail.com>
2022-07-27 18:51:24 -0500

    herencia

---
+++ src/08-inheritance.ts
@@ -0,0 +1,37 @@
+export class Animal {
+    constructor(
+        public name:string
+    ){}
+
+    move() {
+        console.log('Moving');
+    }
+
+    greeting() {
+        return `Hello, I´m ${this.name}`;
+    }
+}
+
+export class Dog extends Animal {
+
+    constructor(
+        name: string,
+        public owner:string
+    ) {
+        super(name);
+    }
+
+    woof(times:number) {
+        for(let index = 0; index < times; index++) {
+            console.log('wooooof');
+        } 
+    }
+}
+
+
+
+const mashu = new Dog('mashu','patto');
+mashu.move();
+console.log(mashu.greeting());
+mashu.woof(5);
+console.log(mashu.owner);
